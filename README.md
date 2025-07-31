2e447b14d9bd605b20b94ce7bc047a4327d0c68c
Ryan6578 <6613922+Ryan6578@users.noreply.github.com>
2025-07-31 18:38:56 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# 🎮 Lobby Lockdown
+
+**Lobby Lockdown** is a community-driven ban list tool designed for the game **Lockdown Protocol** on Steam. It helps players host cleaner, more enjoyable lobbies by allowing them to maintain and share banned players and edit their ban lists outside of the game, since the game doesn't allow that currently. The project includes utilities for managing ban lists, as well as syncing lists from external sources.
+
+> ⚠️ **Disclaimer:** Lobby Lockdown is not affiliated with *Lockdown Protocol*, Mirage Creative Lab, or any associated entity. All data is user-submitted and for informational purposes only. No guarantees are made regarding accuracy. If you believe you were added to the list in error, please submit a request on Discord for review. Please view our full disclaimer [here](https://raw.githubusercontent.com/Lobby-Lockdown/lobby-lockdown/refs/heads/main/DISCLAIMER).
+
+---
+
+## 💬 Join the Community
+
+Have questions, feedback, or want to contribute?  
+Join the **Lobby Lockdown Discord** and connect with fellow players and developers:
+
+[![Join our Discord](https://img.shields.io/discord/1399508907512168730?color=7289DA&label=Discord&logo=discord&style=for-the-badge)](https://discord.gg/Kc9KRBJPMA)
+
+## 🚀 Features
+- View the full list of banned players on your local ban list
+- Add/remove players from your local ban list
+- Import a community-sourced list of banned players to keep your lobbies safe
+
+## Requirements
+To run the project, you'll need:
+- [Node.js](https://nodejs.org/) (v16 or higher recommended)
+- [npm](https://www.npmjs.com/) or [yarn](https://yarnpkg.com/)
+
+## 📦 Installation
+1. Clone the repository:
+```bash
+git clone https://github.com/Lobby-Lockdown/lobby-lockdown.git
+cd lobby-lockdown
+```
+2. Install NPM dependencies
+```bash
+npm install
+```
+3. Run Lobby Lockdown
+```bash
+npm run start
+```
---
+++ src/gvas.js
@@ -0,0 +1,149 @@
+const fs = require('fs');
+
+class GVAS {
+
+    #banListFile;
+    #bannedPlayers;
+
+    // Tracking file contents
+    #startIdx;
+    #endIdx;
+
+    // Hexadecimal patterns to denote the start and end of the banned players array
+    #startPattern = Buffer.from([0x00, 0x00, 0x00, 0x12, 0x00, 0x00, 0x00]);
+    #endPattern = Buffer.from([0x00, 0x05, 0x00, 0x00, 0x00]);
+
+    // Delimiter buffer between each player entry in the array
+    #delimiter = Buffer.from([0x00, 0x12, 0x00, 0x00, 0x00]);
+
+    // Extra buffer pattern that occurs after 3 players that have been banned
+    #extraBuffer = Buffer.from([0x00, 0x02, 0x00, 0x00, 0x00, 0x30]);
+
+    static Error = {
+        FileNotFound: 0,
+        FileNotAccessible: 1,
+        InvalidFileFormat: 2,
+    }
+
+    constructor(filePath) {
+        // Check first to see if the file exists
+        const fileExists = fs.existsSync(filePath);
+
+        if(!fileExists) {
+            throw { code: GVAS.Error.FileNotFound, message: `File doesn't exist: ${filePath}` };
+        }
+        
+        try {
+            // Check to see that the file can be opened as r/w
+            fs.openSync(filePath, 'r+');
+        } catch(error) {
+            throw { code: GVAS.Error.FileNotAccessible, message: `File cannot be read/written: ${filePath}` };
+        }
+
+        this.#banListFile = filePath;
+
+        this.getBanList();
+    }
+
+    getBanList() {
+        const buffer = fs.readFileSync(this.#banListFile);
+
+        // Find the indices of the pattern in the ban list buffer
+        this.#endIdx = this.#findPattern(buffer, this.#endPattern);
+        this.#startIdx = this.#findPattern(buffer, this.#startPattern, this.#endIdx - 1);
+
+        var extracted;
+        if (this.#startIdx !== -1 && this.#endIdx !== -1) {
+            extracted = buffer.slice(this.#startIdx + this.#startPattern.length, this.#endIdx);
+        } else {
+            throw { code: GVAS.Error.InvalidFileFormat, message: `An error occurred parsing the GVAS ban list file. Did not find expected pattern.` };
+        }
+
+        const segments = this.#splitBufferOnDelimiter(extracted, this.#delimiter);
+        const ids = this.#extractFirst64BitsAsString(segments);
+
+        this.#bannedPlayers = ids;
+
+        return this.#bannedPlayers;
+    }
+
+    addPlayersToBanList(players) {
+        const bannedPlayers = this.getBanList();
+
+        const updatedBanList = Array.from(new Set([...bannedPlayers, ...players]));
+
+        const bufferMap = updatedBanList.map(id => Buffer.from(id, 'utf8'));
+
+        this.#updateBanList(bufferMap);
+
+        return updatedBanList.length - bannedPlayers.length;
+    }
+
+    removePlayerFromBanList(playerToRemove) {
+        const bannedPlayers = this.getBanList();
+
+        const updatedBanList = bannedPlayers.filter(steamID => steamID !== playerToRemove);
+
+        const bufferMap = updatedBanList.map(id => Buffer.from(id, 'utf8'));
+
+        this.#updateBanList(bufferMap);
+
+        return bannedPlayers.length - updatedBanList.length;
+    }
+
+    #updateBanList(updatedList) {
+        const buffer = fs.readFileSync(this.#banListFile);
+
+        const mergedData = Buffer.concat(
+            updatedList.flatMap((buf, i, arr) =>
+                i < arr.length - 1 ? (i == 2 && arr.length > 3 ? [buf, this.#extraBuffer, this.#delimiter] : [buf, this.#delimiter]) : [buf]
+            )
+        );
+
+        const before = buffer.slice(0, this.#startIdx + this.#startPattern.length);
+        const after  = buffer.slice(this.#endIdx);
+
+        const updated = Buffer.concat([before, mergedData, after]);
+
+        // Update array length in the buffer - always incremented by 1 past the number of players
+        updated[this.#startIdx - 1] = parseInt((updatedList.length + 1).toString(16), 16);
+
+        // Update array allocation length
+        updated[this.#startIdx - 6] = parseInt(((22 * updatedList.length) + 4 + (updatedList.length > 3 ? 6 : 0)).toString(16), 16);
+
+        fs.writeFileSync(this.#banListFile, updated);
+    }
+
+    #findPattern(buffer, pattern, startIndex = buffer.length - pattern.length) {
+        for (let i = startIndex; i >= 0; i--) {
+            if (buffer.slice(i, i + pattern.length).equals(pattern)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    #splitBufferOnDelimiter(buffer, delimiter) {
+        let parts = [];
+        let start = 0;
+        let index;
+
+        while ((index = buffer.indexOf(delimiter, start)) !== -1) {
+            parts.push(buffer.slice(start, index));
+            start = index + delimiter.length;
+        }
+        parts.push(buffer.slice(start)); // Include the final chunk
+        return parts;
+    }
+
+    #extractFirst64BitsAsString(parts) {
+        return parts.map(part => {
+            if (part.length >= 17) {
+                return part.slice(0, 17).toString('utf8');
+            }
+            return null;
+        }).filter(Boolean);
+    }
+}
+
+module.exports = GVAS;
